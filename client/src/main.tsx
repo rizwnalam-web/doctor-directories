c444ecac40056e382a81ae7291cf1c12a0bd6eb7
Rizwan Alam <63713128+rizwan0612@users.noreply.github.com>
2025-11-01 19:34:31 -0400

    Revert "fixed"
    This reverts commit 017fd5c01fa3f4f0270d48c91cab4f6c4d8a0092.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Failed to find the root element')
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  rootElement
 )
--- client/src/types.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare module 'react-dom/client' {
-    export * from 'react-dom'
-}
