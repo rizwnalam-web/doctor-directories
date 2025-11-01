017fd5c01fa3f4f0270d48c91cab4f6c4d8a0092
Rizwan Alam <63713128+rizwan0612@users.noreply.github.com>
2025-11-01 19:32:39 -0400

    fixed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  rootElement
 )
---
+++ client/src/types.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module 'react-dom/client' {
+    export * from 'react-dom'
+}
