02a088a3181fa02568587955d0387e53288b6007
Rizwan Alam <63713128+rizwan0612@users.noreply.github.com>
2025-11-01 19:36:43 -0400

    Revert "fixed build issue"
    This reverts commit 0a35b3210f144af9d57bef9fad98bb648b9fa316.

--- client/vite.config copy.ts
+++ client/vite.config copy.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://doctor-directories-server.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
